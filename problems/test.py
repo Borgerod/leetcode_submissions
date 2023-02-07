849520c9ed649b7fa2a139af6fd08d35ec5a99c8
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2023-02-07 21:01:01 +0100

    Update test.py

--- problems/test.py
+++ problems/test.py
@@ -15,7 +15,6 @@ class solution:
 	def maxProfit(self, ):
 		print(self.max_price)
 		print(self.min_price)
-		print(self.price_list)
 		if self.checkIfTrue():
 			print(f"{self.price_list.index(self.max_price)} > {self.price_list.index(self.min_price)}")
 			self.result = self.max_price-self.min_price
