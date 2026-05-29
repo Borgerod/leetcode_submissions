4ae760ee8ebcb8286f7fe07b4d502a5886dc52fe
Borgerod <borgerod@hotmail.com>
2026-05-29 13:57:13 +0200

    Create 33. Search in Rotated Sorted Array (/incomplete)   contains: description, solution.   difficulty: Medium   topics: Array, Binary Search

--- problems/incomplete/33_Search_in_Rotated_Sorted_Array/search_in_rotated_sorted_array.py
+++ problems/incomplete/33_Search_in_Rotated_Sorted_Array/search_in_rotated_sorted_array.py
@@ -1,27 +1,74 @@
+# class Solution:
+ 
+#     def search(self, nums: list[int], target: int) -> int:
+#         ''' MERGED '''
+#         left = 0
+#         right = len(nums) - 1
+#         t=-1
+#         while left<right:
+#             mid = left + ((right - left) // 2)
+#             print(f"    split:  {nums[left:mid]} {nums[mid]} {nums[mid:right]}")
+            
+#             if nums[mid] > nums[right] or nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid - 1
+
+#             if nums[mid] == target:
+#                 t = mid
+#         return t
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-        
-        '''
-        
-        '''
+    def findMin(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
 
+            mid = left + ((right - left) // 2)
 
-
-        return None
-
-
-
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            
+            elif nums[mid] < nums[right]:
+                right = mid    
+        return left
     
+    def binarySearch(self, arr, targetVal):
+        left = 0
+        right = len(arr) - 1
 
+        while left <= right:
+            mid = (left + right) // 2
+
+            if arr[mid] == targetVal:
+                return mid
+
+            if arr[mid] < targetVal:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+    def search(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+        rotation = self.findMin(nums, target)
+        _nums = nums[rotation:] + nums[:rotation]
+        ans = self.binarySearch(_nums, target)
+        if ans == -1:
+            return -1
+        return (ans + rotation) % n
+    
 if __name__ == '__main__':
 
     cases = [
         [4,5,6,7,0,1,2],
 		0,
-		[4,5,6,7,0,1,2],
-		3,
-		[1],
-		0
+		[3,5,6,0,1,2],
+        5,
+		# [4,5,6,7,0,1,2],
+		# 3,
+		# [1],
+		# 0
     ]
 
 #> OPTION 2 (FOR MULTIPLE INPUTS)
